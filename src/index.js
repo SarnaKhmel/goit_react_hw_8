aa2be21e2405727b7049a655faec598ce983e451
sarnakhmel <OlSa@MacBook-Pro-username.local>
2023-02-10 04:36:14 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit_react_hw_08">
+        <BrowserRouter basename="/goit_react_hw_8">
           <App />
         </BrowserRouter>
       </PersistGate>
